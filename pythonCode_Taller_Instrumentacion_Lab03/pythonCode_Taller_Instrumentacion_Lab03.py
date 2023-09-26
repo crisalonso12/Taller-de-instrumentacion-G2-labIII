ce5e8ef12a9df5994cd9957d01023fc4493a4432
Navarro98 <navarroveganatalia@gmail.com>
2023-09-26 17:53:01 -0600

    crea tabla usuarios con datos prueba

--- pythonCode_Taller_Instrumentacion_Lab03/pythonCode_Taller_Instrumentacion_Lab03.py
+++ pythonCode_Taller_Instrumentacion_Lab03/pythonCode_Taller_Instrumentacion_Lab03.py
@@ -1,28 +1,28 @@
 import mysql.connector
 
-#conn = mysql.connector.connect(
-#    host="localhost",
-#    user="tu_usuario",
-#    password="tu_contraseña",
-#    database="nombre_de_la_base_de_datos"
-#)
-#
-#cursor = conn.cursor()
-#
-## Crear una tabla
-#cursor.execute('''CREATE TABLE IF NOT EXISTS usuarios (id INT AUTO_INCREMENT PRIMARY KEY, nombre VARCHAR(255), edad INT)''')
-#
-## Insertar datos
-#cursor.execute("INSERT INTO usuarios (nombre, edad) VALUES (%s, %s)", ('Alice', 30))
-#
-## Realizar una consulta
-#cursor.execute("SELECT * FROM usuarios")
-#datos = cursor.fetchall()
-#for fila in datos:
-#    print(fila)
-#
-#conn.commit()
-#conn.close()
+conn = mysql.connector.connect(
+    host="localhost",
+    user="admin",
+    password="ti1234",
+    database="ti_g2"
+)
+
+cursor = conn.cursor()
+
+# Crear una tabla
+cursor.execute('''CREATE TABLE IF NOT EXISTS usuarios (id INT AUTO_INCREMENT PRIMARY KEY, nombre VARCHAR(255), edad INT)''')
+
+# Insertar datos
+cursor.execute("INSERT INTO usuarios (nombre, edad) VALUES (%s, %s)", ('Alice', 30))
+
+# Realizar una consulta
+cursor.execute("SELECT * FROM usuarios")
+datos = cursor.fetchall()
+for fila in datos:
+    print(fila)
+
+conn.commit()
+conn.close()
 
 
 print('Base de datos creada')
